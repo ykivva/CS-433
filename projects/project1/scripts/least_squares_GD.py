e8368850f8ea475b0f3062fc3a8af97f7ace38c1
ykivva <26yaroslav@gmail.com>
2020-09-27 16:56:08 +0200

    style: format docstrings and spaces between functs

--- projects/project1/scripts/least_squares_GD.py
+++ projects/project1/scripts/least_squares_GD.py
@@ -1,47 +1,54 @@
 import numpy as np
 
+
 def compute_least_squares_loss(y, tx, w):
-    '''
-    Compute Least Squares loss
-    PARAM:
-    y: np.array, true predictions
-    tx: np.array, data features
-    w: np.array, model weights
-    RETURN:
-    loss: float
+    '''Compute Least Squares loss
+
+    Args:
+        y (np.array): true predictions
+        tx (np.array): data features
+        w (np.array): model weights
+    
+    Returns:
+        loss (float):
     '''
     n = y.shape[0]
     errors = y - np.dot(tx,w)
     loss = 1./(2*n) * np.dot(errors.T, errors)
     return loss
 
+
 def compute_least_squares_gradient(y, tx, w):
-    '''
-    Compute gradient of Least Squares loss function
-    PARAM:
-    y: np.array, true predictions
-    tx: np.array, data features
-    w: np.array, model weights
-    RETURN:
-    grad: np.array, gradient with respect to model weights
+    '''Compute gradient of Least Squares loss function
+
+    Args:
+        y (np.array): true predictions
+        tx (np.array): data features
+        w (np.array): model weights
+
+    Returns:
+        grad (np.array): gradient with respect to model weights
     '''
     n = y.shape[0]
     error = y - np.dot(tx,w)
     grad = -1./n * np.dot(tx.T, error)
     return grad
 
+
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     '''
     Gradient Descent for Least Squares method
-    PARAM:
-    y: np.array, true predictions
-    tx: np.array, data features
-    initial_w: np.array, initial model weights
-    max_iters: int, number of iterations
-    gamma: float, learning rate parameter
-    RETURN:
-    w: np.array, model weigths after the last iteration
-    loss: float, loss value after the last iteration
+
+    Args:
+        y (np.array): true predictions
+        tx (np.array): data features
+        initial_w (np.array) initial model weights
+        max_iters (int): number of iterations
+        gamma (float): learning rate parameter
+
+    Returns:
+        w (np.array): model weigths after the last iteration
+        loss (float): loss value after the last iteration
     '''
     w = initial_w
     for i in range(max_iters):
--- projects/project1/scripts/least_squares_SGD.py
+++ projects/project1/scripts/least_squares_SGD.py
@@ -1,23 +1,26 @@
 import numpy as np
+
 from least_squares_GD import compute_least_squares_loss, compute_least_squares_gradient
 
+
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
-    '''
-    Stochastic Gradient Descent for Least Squares method using batch size = 1
-    PARAM:
-    y: np.array, true predictions
-    tx: np.array, data features
-    initial_w: np.array, initial model weights
-    max_iters: int, number of iterations
-    gamma: float, learning rate parameter
-    RETURN:
-    w: np.array, model weigths after the last iteration
-    loss: float, loss value after the last iteration on corresponding batch
+    '''Stochastic Gradient Descent for Least Squares method using batch size = 1
+    
+    Args:
+        y (np.array): true predictions
+        tx (np.array): data features
+        initial_w (np.array): initial model weights
+        max_iters (int): number of iterations
+        gamma (float): learning rate parameter
+    Returns:
+        w (np.array): model weigths after the last iteration
+        loss (float): loss value after the last iteration on corresponding batch
     '''
     w = initial_w
     batch_numbers = np.random.randint(0, y.shape[0], size=max_iters)
     for i in batch_numbers:
         grad = compute_least_squares_gradient(y[i:i+1], tx[i:i+1], w)
         w = w - gamma*grad
+    
     loss = compute_least_squares_loss(y[i:i+1], tx[i:i+1], w)
     return (w, loss)

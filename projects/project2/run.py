ce0bdfae7baf8c858a04c06ddc5b94a31009d539
y.kivva <26yaroslav@gmail.com>
2020-12-17 11:07:34 +0000

    feat:Add run and train

---
+++ projects/project2/run.py
@@ -0,0 +1,42 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import time
+
+IMAGE_SIDE = 400
+
+
+from utils import *
+from losses import *
+from models import *
+from model_wrapper import *
+import os 
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+params = {}
+params['input_side'] = 128
+params['image_side'] = IMAGE_SIDE
+params['num_blocks'] = 5
+params['activation_'] = 'elu'
+params['regularizer_'] = None
+params['starting_num_channels'] = 16
+params['metrics'] = [F1_score]
+params['metrics_names'] = ['loss', 'F1_score']
+params['batch_size'] = 16
+params['batches_per_epoch'] = 200
+params['reg_name'] = None
+params['lambd'] = 0
+params['loss'] = soft_dice_loss
+params['main_metric_fn'] = F1_score 
+params['optimizer'] = tf.keras.optimizers.Adam()
+params['path'] = '/saved_models/best' 
+
+model_best = get_Unet_model(params)
+model_best = ModelWrapper(model_best, params)
+
+model_best.load_model()
+
+test_data = load_test_data()
+preds = model_best.predict()
+save_preds(np.round(preds))
---
+++ projects/project2/train.py
@@ -0,0 +1,55 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import time
+
+IMAGE_SIDE = 400
+
+
+from utils import *
+from losses import *
+from models import *
+from model_wrapper import *
+import os 
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+x_train, y_train = load_train_data()
+y_train = transform_labels(y_train)
+
+params = {}
+params['input_side'] = 128
+params['image_side'] = IMAGE_SIDE
+params['num_blocks'] = 5
+params['activation_'] = 'elu'
+params['regularizer_'] = None
+params['starting_num_channels'] = 16
+params['metrics'] = [F1_score]
+params['metrics_names'] = ['loss', 'F1_score']
+params['batch_size'] = 16
+params['batches_per_epoch'] = 200
+params['reg_name'] = None
+params['lambd'] = 0
+params['loss'] = soft_dice_loss
+params['main_metric_fn'] = F1_score 
+params['optimizer'] = tf.keras.optimizers.Adam()
+params['path'] = '/saved_models/best' 
+
+model_best = get_Unet_model(params)
+model_best = ModelWrapper(model_best, params)
+
+model_best.train(x_train, y_train, epochs=300, rot_angle=45)
+
+new_lr = 0.1 * model_best.model.optimizer.new_lr
+K.set_value(model_best.model.optimizer.new_lr, new_lr)
+
+model_best.train(x_train, y_train, epochs=150, rot_angle=45)
+
+new_lr = 0.1 * model_best.model.optimizer.new_lr
+K.set_value(model_best.model.optimizer.new_lr, new_lr)
+
+model_best.train(x_train, y_train, epochs=50, rot_angle=45)
+
+test_data = load_test_data()
+preds = model_best.predict()
+save_preds(np.round(preds))
